17f7cd7283097464934804ae1276850d64b9e2b5
Alessandra Filippi <filippi@to.infn.it>
2020-05-07 09:37:37 +0200

    update

--- examplesAndExercises/1_Simple_detector/ex1_3_Xrays/geometry_xRays.py
+++ examplesAndExercises/1_Simple_detector/ex1_3_Xrays/geometry_xRays.py
@@ -18,24 +18,24 @@ def makeGeometry(configuration):
 
 
 	# volume fields can be given either as named arguments in the MyFinger() call or  assigned later to the instance variable
-	finger = MyDetector(name="finger", mother="root")	
-	finger.description = "finger"
-	finger.color       = "FFA07A"
+	flesh = MyDetector(name="flesh", mother="root")	
+	flesh.description = "flesh"
+	flesh.color       = "FFA07A"
 	# Tube shape dimensions are:  inner_radius, outer_radius, half-length, starting_angle, total angle
 	# A non-zero inner radius will produce a hollow tube.  The angles allow for an angular cut in the cross section
-	finger.type        = "Tube"
-	finger.dimensions = "0.5*cm 0.8*cm 5.*cm 0*deg 360*deg"
-        finger.pos = "0.*cm 0.*cm 0.*cm"
-        finger.rotation = "90.*deg 0.*deg 0.*deg"                                                
-	finger.material   = "G4_MUSCLE_SKELETAL_ICRP"	# G4_Si is a GEANT4 defined element name
+	flesh.type        = "Tube"
+	flesh.dimensions = "0.5*cm 0.8*cm 5.*cm 0*deg 360*deg"
+        flesh.pos = "0.*cm 0.*cm 0.*cm"
+        flesh.rotation = "90.*deg 0.*deg 0.*deg"                                                
+	flesh.material   = "G4_MUSCLE_SKELETAL_ICRP"	# G4_Si is a GEANT4 defined element name
 							# When using only built-in materials, a separate materials file is not needed
 
-	finger.visible = 1			# 1 to display volume with the full geometry, 0 to leave hidden
-	finger.style = 1			    # 1 displays volume as a solid, 0 displays as wireframe
-	finger.sensitivity = "flux"	# Every track through the volume will generate a hit
-	finger.hit_type = "flux"		# Every track through the volume will generate a hit
-	finger.identifiers = "1"		# Identifies the detector being hit:  for FLUX detector this is an integer value
-	print_det(configuration, finger)
+	flesh.visible = 1			# 1 to display volume with the full geometry, 0 to leave hidden
+	flesh.style = 1			    # 1 displays volume as a solid, 0 displays as wireframe
+	flesh.sensitivity = "flux"	# Every track through the volume will generate a hit
+	flesh.hit_type = "flux"		# Every track through the volume will generate a hit
+	flesh.identifiers = "1"		# Identifies the detector being hit:  for FLUX detector this is an integer value
+	print_det(configuration, flesh)
 
 
         bone = MyDetector(name="bone", mother="root")
@@ -47,7 +47,8 @@ def makeGeometry(configuration):
         bone.dimensions = "0.*cm 0.5*cm 4.5*cm 0*deg 360*deg"
 	bone.pos = "0.*cm -0.5*cm 0.*cm"
         bone.rotation = "90.*deg 0.*deg 0.*deg"
-        bone.material   = "G4_BONE_COMPACT_ICRU" # G4_Si is a GEANT4 defined element name
+#        bone.material   = "G4_BONE_COMPACT_ICRU" # G4_Si is a GEANT4 defined element name
+	bone.material = "G4_Fe"
                                                         # When using only built-in materials, a separate materials file is not needed
         bone.visible = 1                    # 1 to display volume with the full geometry, 0 to leave hidden
         bone.style = 1                          # 1 displays volume as a solid, 0 displays as wireframe
