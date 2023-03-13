26dff877262f3b8cfe22f9570793c8c3df2840cf
Alessandra Filippi <filippi@to.infn.it>
2023-03-13 12:21:52 +0100

    INSPYRE2023 material update

---
+++ INSPYRE23/launchNOVNC.sh
@@ -0,0 +1,2 @@
+#/bin/sh
+docker run -it --rm -p 6080:6080 -v /home/filippi/Software/gemc/2023/geant4course/INSPYRE23:/jlab/work/myWork jeffersonlab/gemcinteractive:2.7
---
+++ INSPYRE23/steelShell/geometry.py
@@ -0,0 +1,36 @@
+from gemc_api_geometry import *
+
+def makeGeometry(configuration):
+	# volume fields can be given either as named arguments in the steelShell() call or  assigned later to the instance variable
+	steelShell = MyDetector(name="steelShell", mother="root")	
+	steelShell.description = "stainless steel shell"
+	steelShell.color       = "C0C0C0"
+	# Tube shape dimensions are:  inner_radius, outer_radius, half-length, starting_angle, total angle
+	# A non-zero inner radius will produce a hollow tube.  The angles allow for an angular cut in the cross section
+	steelShell.type        = "Sphere"
+	steelShell.dimensions = "4.5*cm  5.*cm  0*deg 360*deg 0*deg 180*deg"
+	steelShell.material   = "G4_STAINLESS-STEEL"	# G4_Si is a GEANT4 defined element name
+							# When using only built-in materials, a separate materials file is not needed
+	steelShell.visible = 1			# 1 to display volume with the full geometry, 0 to leave hidden
+	steelShell.style = 1			    # 1 displays volume as a solid, 0 displays as wireframe
+	steelShell.sensitivity = "flux"	# Every track through the volume will generate a hit
+	steelShell.hit_type = "flux"		# Every track through the volume will generate a hit
+	steelShell.identifiers = "1"		# Identifies the detector being hit:  for FLUX detector this is an integer value
+	print_det(configuration, steelShell)
+
+	# volume fields can be given either as named arguments in the steelShell() call or  assigned later to the instance variable
+	hollowAir = MyDetector(name="hollowAir", mother="root")	
+	hollowAir.description = "hollow void"
+	hollowAir.color       = "ffffff"
+	# Tube shape dimensions are:  inner_radius, outer_radius, half-length, starting_angle, total angle
+	# A non-zero inner radius will produce a hollow tube.  The angles allow for an angular cut in the cross section
+	hollowAir.type        = "Sphere"
+	hollowAir.dimensions = "0.*cm  4.5*cm  0*deg 360*deg 0*deg 180*deg"
+	hollowAir.material   = "G4_Galactic"	# G4_Si is a GEANT4 defined element name
+							# When using only built-in materials, a separate materials file is not needed
+	hollowAir.visible = 1			# 1 to display volume with the full geometry, 0 to leave hidden
+	hollowAir.style = 1			    # 1 displays volume as a solid, 0 displays as wireframe
+	hollowAir.sensitivity = "flux"	# Every track through the volume will generate a hit
+	hollowAir.hit_type = "flux"		# Every track through the volume will generate a hit
+	hollowAir.identifiers = "1"		# Identifies the detector being hit:  for FLUX detector this is an integer value
+	print_det(configuration, hollowAir)
