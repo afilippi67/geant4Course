eca7b639a213611e3aaae0a5473270e8f23987d7
afilippi67 <root@localhost.localdomain>
2023-03-22 19:01:38 +0100

    INSPYRE23 exercises

--- INSPYRE23/humanBody/testEyeDecomposed/geometry.py
+++ INSPYRE23/humanBody/testEyeDecomposed/geometry.py
@@ -1,124 +1,139 @@
 from gemc_api_geometry import *
 
 def makeGeometry(configuration):
-	# volume fields can be given either as named arguments in the MyDetector() call or  assigned later to the instance variable
 	irisDisk = MyDetector(name="irisDisk", mother="root")	
 	irisDisk.description = "iris disk"
 	irisDisk.color       = "00bfff"
-	# Tube shape dimensions are:  inner_radius, outer_radius, half-length, starting_angle, total angle
-	# A non-zero inner radius will produce a hollow tube.  The angles allow for an angular cut in the cross section
 	irisDisk.type        = "Tube"
-	irisDisk.dimensions = "0.15*mm 0.71*mm 0.01*mm 0*deg 360*deg"
-	irisDisk.pos = "(0., 0., 1.85)mm"
-	irisDisk.material   = "G4_WATER"	# G4_Si is a GEANT4 defined element name
-							# When using only built-in materials, a separate materials file is not needed
-	irisDisk.visible = 1			# 1 to display volume with the full geometry, 0 to leave hidden
-	irisDisk.style = 1			    # 1 displays volume as a solid, 0 displays as wireframe
-	irisDisk.sensitivity = "flux"	# Every track through the volume will generate a hit
-	irisDisk.hit_type = "flux"		# Every track through the volume will generate a hit
-	irisDisk.identifiers = "1"		# Identifies the irisDisk being hit:  for FLUX irisDisk this is an integer value	
+	irisDisk.dimensions = "0.15*mm 0.72*mm 0.005*mm 0*deg 360*deg"
+	irisDisk.pos = "-0.005*mm 0.*mm 1.825*mm"
+	irisDisk.material   = "G4_WATER"
+        irisDisk.visible = "1"
+        irisDisk.style = "1"
+	irisDisk.sensitivity = "flux"	
+	irisDisk.hit_type = "flux"	
+	irisDisk.identifiers = "1"	
 	print_det(configuration, irisDisk)
-
 
         pupilDisk = MyDetector(name="pupilDisk", mother="root")
         pupilDisk.description = "pupil disk"
         pupilDisk.color       = "000000"
-        # Tube shape dimensions are:  inner_radius, outer_radius, half-length, $
-        # A non-zero inner radius will produce a hollow tube.  The angles allow$
         pupilDisk.type        = "Tube"
         pupilDisk.dimensions = "0.*mm 0.15*mm 0.01*mm 0*deg 360*deg"
-        pupilDisk.pos = "(0., 0., 1.85)mm"
-        pupilDisk.material   = "G4_WATER"        # G4_Si is a GEANT4 defined ele$
-                                                        # When using only built$
-        pupilDisk.visible = 1                    # 1 to display volume with the $
-        pupilDisk.style = 1                          # 1 displays volume as a so$
-        pupilDisk.hit_type = "flux"              # Every track through the volum$
-        pupilDisk.identifiers = "1"              # Identifies the pupilDisk being$
+        pupilDisk.pos = "-0.005*mm 0.*mm 1.825*mm"
+        pupilDisk.material   = "G4_WATER"  
+        pupilDisk.visible = 1
+        pupilDisk.style = 1  
+        pupilDisk.hit_type = "flux"
+        pupilDisk.identifiers = "1"
 	print_det(configuration, pupilDisk)	
 
         retina = MyDetector(name="retina", mother="root")
         retina.description = "retina"
         retina.color       = "8000802"
-        # Tube shape dimensions are:  inner_radius, outer_radius, half-length, $
-        # A non-zero inner radius will produce a hollow tube.  The angles allow$
         retina.type        = "Sphere"
         retina.dimensions = "1.895*mm 2.*mm 90.*deg 180.*deg 0.*deg 360.*deg"
         retina.pos = "(0., 0., 0.)mm"
 #	retina.rotation="(90., 90., -90.)*deg"
 	retina.rotation="0.*deg 90.*deg 0.*deg"
-        retina.material   = "G4_WATER"        # G4_Si is a GEANT4 defined ele$
-                                                        # When using only built$
-        retina.visible = 1                    # 1 to display volume with the $
-        retina.style = 1                          # 1 displays volume as a so$
+        retina.material   = "G4_WATER"        
+        retina.visible = 1                   
+        retina.style = 1                      
   	retina.sensitivity = "flux"
-        retina.hit_type = "flux"              # Every track through the volum$
-        retina.identifiers = "1"              # Identifies the pupilDisk being$
+        retina.hit_type = "flux"              
+        retina.identifiers = "1"              
 	print_det(configuration, retina)
 
-#        frontSkinUp = MyDetector(name="frontSkinUp", mother="root")
-#        frontSkinUp.description = "frontSkinUp"
-#        frontSkinUp.color       = "f0ffff2"
-#        # Tube shape dimensions are:  inner_radius, outer_radius, half-length, $
-#        # A non-zero inner radius will produce a hollow tube.  The angles allow$
-#        frontSkinUp.type        = "Sphere"
-#	frontSkinUp.dimensions = "1.895*mm 2.*mm 20.*deg 70.*deg 0.*deg 360.*deg"
-#        frontSkinUp.pos = "(0., 0., 0.)mm"
-#	frontSkinUp.rotation="0.*deg 90.*deg 0.*deg"
-#        frontSkinUp.material   = "G4_WATER"        # G4_Si is a GEANT4 defined ele$
-#                                                        # When using only built$
-#        frontSkinUp.visible = 1                    # 1 to display volume with the $
-#        frontSkinUp.style = 1                          # 1 displays volume as a so$
-#        frontSkinUp.hit_type = "flux"              # Every track through the volum$
-#        frontSkinUp.identifiers = "1"              # Identifies the pupilDisk being$
-#	print_det(configuration, frontSkinUp)
-	
-#        frontSkinDown = MyDetector(name="frontSkinDown", mother="root")
-#        frontSkinDown.description = "frontSkinDown"
-#        frontSkinDown.color       = "f0ffff2"
-#        # Tube shape dimensions are:  inner_radius, outer_radius, half-length, $
-#        # A non-zero inner radius will produce a hollow tube.  The angles allow$
-#        frontSkinDown.type        = "Sphere"
-#	frontSkinDown.dimensions = "1.895*mm 2.*mm  90.*deg 120.*deg 0.*deg 360.*deg"
-#        frontSkinDown.pos = "(0., 0., 0.)mm"
-#	frontSkinDown.rotation="0.*deg 90.*deg 0.*deg"
-#        frontSkinDown.material   = "G4_WATER"        # G4_Si is a GEANT4 defined ele$
-                                                        # When using only built$
-#        frontSkinDown.visible = 1                    # 1 to display volume with the $
-#        frontSkinDown.style = 1                          # 1 displays volume as a so$
-#        frontSkinDown.hit_type = "flux"              # Every track through the volum$
-#        frontSkinDown.identifiers = "1"              # Identifies the pupilDisk being$
-#	print_det(configuration, frontSkinDown)
 
-#        outerSkin = MyDetector(name="outerSkin", mother="root")
-#        outerSkin.description = "outerSkin"
-#        outerSkin.color       = "FFFAFA4"
-#        # Tube shape dimensions are:  inner_radius, outer_radius, half-length, $
-#        # A non-zero inner radius will produce a hollow tube.  The angles allow$
-#        outerSkin.type        = "Sphere"
-#        outerSkin.dimensions = "2*mm 2.01*mm 0.*deg 180.*deg 0.*deg 180.*deg"
-#        outerSkin.pos = "(0., 0., 0.)mm"
-#	outerSkin.rotation="(90., 90., -90.)*deg"
-#        outerSkin.material   = "G4_WATER"        # G4_Si is a GEANT4 defined ele$
-#                                                        # When using only built$
-#        outerSkin.visible = 1                    # 1 to display volume with the $
-#        outerSkin.style = 1                          # 1 displays volume as a so$
-#        outerSkin.hit_type = "flux"              # Every track through the volum$
-#        outerSkin.identifiers = "1"              # Identifies the pupilDisk being$
-#	print_det(configuration, outerSkin)
+# first membrane, only half front part
+        outerSkinFrontFull = MyDetector(name="outerSkinFrontFull", mother="root")
+        outerSkinFrontFull.description = "outerSkin full solid"
+        outerSkinFrontFull.color       = "fff5ee2"
+        outerSkinFrontFull.type        = "Ellipsoid"
+        outerSkinFrontFull.dimensions = "2.0*mm 2.0*mm 2.0*mm  0.*mm 1.825*mm"
+        outerSkinFrontFull.pos = "(0., 0., 0.)mm"
+        outerSkinFrontFull.material   = "Component"
+        outerSkinFrontFull.visible = 0 
+        outerSkinFrontFull.style = 1   
+        #outerSkinFrontFull.hit_type = "flux" 
+        #outerSkinFrontFull.identifiers = "1"  
+	#outerSkinFrontFull.sensitivity = "flux"
+        print_det(configuration, outerSkinFrontFull)
+
+        outerSkinFrontHole = MyDetector(name="outerSkinFrontHole", mother="root")
+        outerSkinFrontHole.description = "outerSkin to be subtracted"
+        outerSkinFrontHole.color       = "fff5ee2"
+        outerSkinFrontHole.type        = "Ellipsoid"
+        outerSkinFrontHole.dimensions = "1.88*mm 1.88*mm 1.88*mm  0.*mm 1.825*mm"
+        outerSkinFrontHole.pos = "(0., 0., 0.)mm"
+        outerSkinFrontHole.material   = "Component"
+        outerSkinFrontHole.visible = 0 
+        #outerSkinFrontHole.style = 1   
+        #outerSkinFrontHole.hit_type = "flux" 
+        #outerSkinFrontHole.identifiers = "1"  
+	#outerSkinFrontHole.sensitivity = "flux"
+        print_det(configuration, outerSkinFrontHole)
 
         outerSkinFront = MyDetector(name="outerSkinFront", mother="root")
-        outerSkinFront.description = "outerSkin"
-        outerSkinFront.color       = "fff5ee2"
-        # Tube shape dimensions are:  inner_radius, outer_radius, half-length, $
-        # A non-zero inner radius will produce a hollow tube.  The angles allow$
-        outerSkinFront.type        = "Ellipsoid"
-        outerSkinFront.dimensions = "2.01*mm 2.01*mm 2.01*mm  0*mm 1.8*mm"
+        outerSkinFront.description = "outerSkin from subtraction"
+        outerSkinFront.color       = "fff5ee3"
+        outerSkinFront.type        = "Operation: outerSkinFrontFull - outerSkinFrontHole"
+        #outerSkinFront.type        = "Ellipsoid"
+        #outerSkinFront.dimensions = "1.98*mm 1.98*mm 1.98*mm  0*mm 1.8*mm"
         outerSkinFront.pos = "(0., 0., 0.)mm"
-        outerSkinFront.material   = "G4_WATER"        # G4_Si is a GEANT4 defined ele$
-                                                         # When using only built$
-        outerSkinFront.visible = 1                    # 1 to display volume with the $
-        outerSkinFront.style = 1                          # 1 displays volume as a so$
-        outerSkinFront.hit_type = "flux"              # Every track through the volum$
-        outerSkinFront.identifiers = "1"              # Identifies the pupilDisk being$
+        outerSkinFront.material   = "G4_WATER"
+        outerSkinFront.visible = 1 
+        outerSkinFront.style = 1   
+        outerSkinFront.hit_type = "flux" 
+        outerSkinFront.identifiers = "1"  
 	outerSkinFront.sensitivity = "flux"
- 	print_det(configuration, outerSkinFront)
+        print_det(configuration, outerSkinFront)
+
+        
+# outer membrane, full eyeball except cornea
+        scleraFull = MyDetector(name="scleraFull", mother="root")
+        scleraFull.description = "outerSkin full solid"
+        scleraFull.type        = "Ellipsoid"
+        scleraFull.dimensions = "2.02*mm 2.02*mm 2.02*mm  -2.02*mm 1.825*mm"
+        scleraFull.pos = "(0., 0., 0.)mm"
+        scleraFull.material   = "Component"
+        scleraFull.visible = 1 
+        scleraFull.style = 1   
+        print_det(configuration, scleraFull)
+
+        scleraHole = MyDetector(name="scleraHole", mother="root")
+        scleraHole.description = "sclera hole to be subtracted"
+        scleraHole.type        = "Ellipsoid"
+        scleraHole.dimensions = "2.0*mm 2.0*mm 2.0*mm  -2.0*mm 1.825*mm"
+        scleraHole.pos = "(0., 0., 0.)mm"
+	scleraHole.material   = "Component"
+        scleraHole.visible = 0 
+        print_det(configuration, scleraHole)
+        sclera = MyDetector(name="sclera", mother="root")
+        sclera.description = "outerSkin from subtraction"
+        sclera.color       = "ffffff2"
+        sclera.type        = "Operation: scleraFull - scleraHole"
+        sclera.pos = "(0., 0., 0.)mm"
+        sclera.material   = "G4_WATER"
+        sclera.visible = 1 
+        sclera.style = 1   
+        sclera.hit_type = "flux" 
+        sclera.identifiers = "4"  
+	sclera.sensitivity = "flux"
+        print_det(configuration, sclera)
+
+# front cornea
+        frontCornea = MyDetector(name="frontCornea", mother="root")
+        frontCornea.description = "front cornea skin"
+        frontCornea.color       = "afeeee4"
+        #outerSkinFront.type        = "Operation: outerSkinFrontFull - outerSkinFrontHole"
+        frontCornea.type        = "Ellipsoid"
+        frontCornea.dimensions = "2.015*mm 2.015*mm 2.015*mm  1.825*mm 2.15*mm"
+        frontCornea.pos = "(0., 0., 0.)mm"
+        frontCornea.material   = "G4_WATER"
+        frontCornea.visible = 1 
+        frontCornea.style = 1   
+        frontCornea.hit_type = "flux" 
+        frontCornea.identifiers = "1"  
+	frontCornea.sensitivity = "flux"
+        print_det(configuration, frontCornea)
